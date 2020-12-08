82fc54547a3277f76bb1822063ba40a5f4866655
Catalin191 <62592391+Catalin191@users.noreply.github.com>
2020-12-08 21:40:02 +0200

    Update 08/12/2020

--- src/main/java/com/example/cmsshoppingcart/models/PageRepository.java
+++ src/main/java/com/example/cmsshoppingcart/models/PageRepository.java
@@ -2,10 +2,20 @@ package com.example.cmsshoppingcart.models;
 
 import com.example.cmsshoppingcart.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
 
+    Page findBySlug(String slug);
+
+//    @Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+//    Page findBySlug(int id, String slug);
+
+    Page findBySlugAndIdNot(String slug, int id); // do the same as the lines 12, 13
+
+    List<Page> findAllByOrderBySortingAsc();
 
 }
