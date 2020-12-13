e79f8324477d7a735e250805efe2ef42e4612765
Catalin191 <62592391+Catalin191@users.noreply.github.com>
2020-12-13 19:13:59 +0200

    Update 13/12/2020 Added products

---
+++ src/main/java/com/example/cmsshoppingcart/models/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.example.cmsshoppingcart.models;
+
+import com.example.cmsshoppingcart.models.data.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Product findBySlug(String slug);
+
+    Product findBySlugAndIdNot(String slug, int id);
+
+    Page<Product> findAll(Pageable pageable);
+}
--- src/main/java/com/example/cmsshoppingcart/webCinfig/Config.java
+++ src/main/java/com/example/cmsshoppingcart/webCinfig/Config.java
@@ -1,6 +1,7 @@
 package com.example.cmsshoppingcart.webCinfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,12 @@ public class Config implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/media/**")
+                .addResourceLocations("file:/C:/Users/Catalin/Documents/StudiuIndividual//Udemy spring boot project/spring boot first project/src/main/resources/static/media/");
     }
 }
